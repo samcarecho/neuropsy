b9125e959f34170eceecfbaf58d6c3407a04a80a
Sam Carecho <digiwise@gmail.com>
2014-12-20 13:51:58 -0200

    initial commit

---
+++ data/patients.js
@@ -0,0 +1,61 @@
+/**
+ * Created by dwnet on 20/12/2014.
+ */
+"use strict";
+
+var patients = [
+  {
+    pName: "A.C.T.H.",
+    pDoB: "1986-11-14",
+    pGender: "masculino",
+    pCity: "Teófilo Otoni",
+    pState: "MG",
+    pEducation: "Superior em Medicina(em curso)",
+    pCase: "Dep. Química"
+  },
+  {
+    pName: "A.E.A",
+    pDoB: "1980-07-21",
+    pGender: "masculino",
+    pCity: "São Paulo",
+    pState: "SP",
+    pEducation: "Superior em Engenharia",
+    pCase: "Dep. Química"
+  },
+  {
+    pName: "D.A.",
+    pDoB: "1980-07-21",
+    pGender: "masculino",
+    pCity: "Campinas",
+    pState: "SP",
+    pEducation: "Médio",
+    pCase: "Degenerativa"
+  },
+  {
+    pName: "G.A.B.A",
+    pDoB: "1980-12-06",
+    pGender: "masculino",
+    pCity: "Porto Alegre",
+    pState: "RS",
+    pEducation: "Superior em Psicologia",
+    pCase: "Dep. Química"
+  },
+  {
+    pName: "H.T",
+    pDoB: "2004-01-25",
+    pGender: "female",
+    pCity: "São Paulo",
+    pState: "SP",
+    pEducation: "Fundamental(em curso)",
+    pCase: "Depressão"
+  },
+  {
+    pName: "N.E",
+    pDoB: "2006-05-18",
+    pGender: "masculino",
+    pCity: "Rio de Janeiro",
+    pState: "RJ",
+    pEducation: "Fundamental",
+    pCase: "DDA"
+  }
+];
